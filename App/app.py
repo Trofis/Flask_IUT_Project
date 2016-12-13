86d16732373df84adab4815c897f57aacac9f38f
Valentin Patronat <patronat@info22-09.iut45.univ-orleans.fr>
2016-12-13 09:39:16 +0100

    Template Boostrap Integration

--- App/app.py
+++ App/app.py
@@ -1,7 +1,12 @@
+#-------------------- Flask --------------------#
 from flask import Flask
+from flask_script import Manager
+
 app = Flask(__name__)
 app.debug = True
 
-from flask_script import Manager
-
 manager = Manager(app)
+#-------------------- Bootstrap --------------------#
+from flask_bootstrap import Bootstrap
+app.config['BOOTSTRAP_SERVE_LOCAL']=True
+Bootstrap(app)
